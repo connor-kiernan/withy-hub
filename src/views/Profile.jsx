d0f02c764f50464dcf3ebf0a7063aeea3263cb1b
connor-kiernan <connorkierna@outlook.com>
2024-04-14 12:00:55 +0100

    Add logout button

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -1,8 +1,25 @@
-const Profile = () => (
-    <>
-      <h1>Profile</h1>
-      <p></p>
-    </>
-);
+import {Button} from "react-bootstrap";
+import {useSendLogoutMutation} from "../features/auth/authApiSlice";
+
+const Profile = () => {
+  const [logout] = useSendLogoutMutation();
+
+  const sendLogout = async () => {
+    await logout();
+
+    window.location.reload();
+  }
+
+  return (
+      <>
+        <div className="text-center">
+          <h1>Profile</h1>
+        </div>
+        <div className="flex-grow-1 d-flex flex-column mt-5">
+          <Button onClick={() => sendLogout()}>Log Out</Button>
+        </div>
+      </>
+  );
+};
 
 export default Profile;
